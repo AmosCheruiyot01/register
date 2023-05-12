9fcadddd066a60bf93d7c50cad4c4ee91480f134
AmosCheruiyot01 <cheruiyotamos728@gmail.com>
2023-05-12 17:25:39 +0300

    1st commit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const list = document.querySelector('.registared ul');
+  const forms = document.forms;
+
+  //   delete
+  list.addEventListener('click', (e) => {
+    if (e.target.className == 'delete') {
+      const li = e.target.parentElement;
+      li.parentNode.removeChild(li);
+    }
+  });
+});
